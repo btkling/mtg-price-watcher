ed58c15470278db79b45b608fa5ef5acf1187e9f
Ben Kling <kling.ben@gmail.com>
2022-06-25 09:37:56 -0400

    add price_analysis.py

---
+++ price_analysis.py
@@ -0,0 +1,16 @@
+
+# TODO read in the price_tracker file
+
+# TODO analyze card trends
+
+# TODO print records with a desired price
+
+# TODO predict cards that will achieve desired price in next n days
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
