b4aea38db87dd998106f77aa6c1ea197dbe6f7cd
Ben Kling <kling.ben@gmail.com>
2022-06-27 11:02:29 -0400

    read price tracker

--- price_analysis.py
+++ price_analysis.py
@@ -1,5 +1,13 @@
+import pandas as pd
+from datetime import datetime as dt
 
-# TODO read in the price_tracker file
+def read_price_data(filepath=None):
+    if filepath:
+        df = pd.read_csv(filepath + "price_tracker.csv")
+    else:
+        df = pd.read_csv("price_tracker.csv")
+
+    return df
 
 # TODO analyze card trends
 
@@ -9,8 +17,10 @@
 
 
 def main():
-
-
+    print("hello")
+    fp = "/mnt/e/git/mtg-price-watcher/"
+    price_history = read_price_data()
+    print(price_history.head())
 
 if __name__ == "__main__":
     main()
