50282be6475b128b35d8ec068c36b938abc4b7e3
Ben Kling <kling.ben@gmail.com>
2022-07-18 11:27:46 -0400

    very basic connection successful

--- emailer.py
+++ emailer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import price_analysis as pran
 import smtplib 
+import ssl
 import json
 
 def load_config(file_path):
@@ -27,6 +28,18 @@ def main():
     print(jscfg)
     for k,v in jscfg.items():
         print(f"Key {k} || Value {v}")
+
+
+    port = 465 # for SSL
+    email = jscfg["email"]
+    password = jscfg["password"]
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(email, password)
+        print("yay")
+        server.sendmail(email, email, f"Subject: HELLO!\nBody of email.\n I am a test.")
 
 if __name__ == "__main__":
     main()
