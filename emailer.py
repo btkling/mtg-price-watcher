ab5993a3f0b302ea57f1150a67921cd7f00bb9d0
Ben Kling <kling.ben@gmail.com>
2022-07-18 11:18:06 -0400

    set up config file parser

--- emailer.py
+++ emailer.py
@@ -1,8 +1,32 @@
 import pandas as pd
 import price_analysis as pran
+import smtplib 
+import json
+
+def load_config(file_path):
+    '''Load the config file for email account
+    
+    # Parameters
+    
+    file_path : str
+        the path to the file
+    
+    # Returns
+    
+    a dictionary with the parsed json'''
+
+    file_handler = open(file_path)
+    jscfg = json.load(file_handler)
+
+    return jscfg
 
 def main():
     print("hi")
+
+    jscfg = load_config("config.json")
+    print(jscfg)
+    for k,v in jscfg.items():
+        print(f"Key {k} || Value {v}")
 
 if __name__ == "__main__":
     main()
